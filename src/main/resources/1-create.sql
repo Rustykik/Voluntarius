562b504bf3f36a47f873c1a89bc270e198a57bf7
Rustykik <valentin.dubr@gmail.com>
2022-04-27 17:16:40 +0300

    looks like working

--- src/main/resources/1-create.sql
+++ src/main/resources/1-create.sql
@@ -2,22 +2,22 @@ drop table if exists users;
 drop table if exists events;
 create table users
 (
-    id INT not null primary key AUTO_INCREMENT,
+    id SERIAL primary key,
     firstname VARCHAR (50),
     lastname VARCHAR (50),
-    login VARCHAR (50) unique,
+    login VARCHAR (50) ,
     passwd VARCHAR (50),
     email VARCHAR (50)
 );
 
 create table event_table
 (
-    id INT not null primary key AUTO_INCREMENT,
+    id SERIAL primary key,
     owner_id INT not null REFERENCES users(id),
     eventName VARCHAR (50),
     description VARCHAR (400),
-    eventStart DATETIME,
-    eventEnd DATETIME,
+    eventStart TIMESTAMP ,
+    eventEnd TIMESTAMP ,
     location VARCHAR (50),
     likes INT
 );
@@ -25,7 +25,7 @@ create table event_table
 -- add unique pair
 create table subscribed
 (
-    user_id LONG REFERENCES users(id),
-    event_id LONG REFERENCES event_table(id)
+    user_id SERIAL REFERENCES users(id),
+    event_id SERIAL REFERENCES event_table(id)
 );
 
