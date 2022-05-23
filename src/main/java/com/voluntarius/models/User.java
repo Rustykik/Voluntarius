6411cb09ec33e2adef802f80fe84e82c4297b59c
Rustykik <valentin.dubr@gmail.com>
2022-05-23 07:02:52 +0300

    add constructors to user model
    need to change lists to sets  and need to change row mappers to lambdas with callback
    also need to make user class immutable
    need make subscribed created events something like HashSet.newHashset ... and then fill it but it will make our class mutable need to think whats better

--- src/main/java/com/voluntarius/models/User.java
+++ src/main/java/com/voluntarius/models/User.java
@@ -1,11 +1,15 @@
 package com.voluntarius.models;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Collections;
 import java.util.List;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
     private Integer id;
     private String firstname;
